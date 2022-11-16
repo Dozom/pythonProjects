e4a8dcba0114a1c77683afeee9dfde53f5c4ff69
Daniel Náger Carpio <dnagerc@inscastellet.cat>
2022-11-16 19:51:58 +0100

    Practica de criptografia asimetrica

---
+++ encriptacioAsimetrica/README.md
@@ -0,0 +1,7 @@
+#Clau Asimetrica
+##Funcionament
+1. Cada usuari té dues claus, una pública i una privada.
+2. L'emissor xifra un missatge amb la clau publica del receptor.
+3. El receptor desxifra el missatge amb la seva clau privada.
+
+
--- encriptacioAsimetrica/practica5.py
+++ encriptacioAsimetrica/practica5.py
@@ -1,35 +1,49 @@
-#import
+import Crypto
+from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
-#########################################################
-#                GENERACIÓN DE LA CLAVE                 #
-#########################################################
+def writeFile(file, content):
+    with open(file,"w") as f:
+        f.write(content)
 
-# Generar pareja de claves RSA de 2048 bits de longitud
-key = RSA.generate(2048)
+def readFile(file):
+    with open(file,"r") as f:
+        return f.read()
 
-# Passphrase para encriptar la clave privada
-secret_code = "12345"
+def generateKeyPair():
+    random_generator = Crypto.Random.new().read
+    private_key = RSA.generate(1024, random_generator)
+    public_key = private_key.publickey().export_key().decode()
+    private_key = private_key.export_key().decode()
+    return public_key, private_key
 
-# Exportamos la clave privada
-private_key = key.export_key(passphrase=secret_code)
+def cipher(public_key, text):
+    cipher = PKCS1_OAEP.new(key=public_key)
+    return cipher.encrypt(text)
 
-# Guardamos la clave privada en un fichero
-with open("private.pem", "wb") as f:
-    f.write(private_key)
+def uncipher(private_key, cipheredtext):
+    cipher = PKCS1_OAEP.new(key=private_key)
+    return cipher.decrypt(cipheredtext)
 
-# Obtenemos la clave pública
-public_key = key.publickey().export_key()
+def keysFromFiles():
+    puk = RSA.import_key(readFile("public_key.pem"))
+    prk = RSA.import_key(readFile("private_key.pem"))
 
-# Guardamos la clave pública en otro fichero
-with open("public.pem", "wb") as f:
-    f.write(public_key)
-# fer-ho amb rsa
-# cr ar parells de claus i xifrar/desxifrar documents
-# funcions independents sense variables globals
-# 1. una funcio que escrigui a un fitxer una clau publica
-# 2. una funcio que llegeixi la clau publica 
-# 3. una funcio que xifra(clau, text)
-# 4. una funcio que desxifra(clau, text)
-# 5. una funcio que executi un exemple de funcionament en mode consola
+def demoTest():
+    text = b'hola'
+    #generate Keys
+    public_key, private_key = generateKeyPair()
+    #write Keys into files
+    writeFile("public_key.pem",public_key)
+    writeFile("private_key.pem",private_key)
+    #cipheredText
+    textXifrat = cipher(puk, text)
+    print("El text xifrat és: ", textXifrat)
+    #uncipheredText
+    textDesxifrat = uncipher(prk, textXifrat)
+    print("El text desxifrat és: ",textDesxifrat.decode())
+
+if __name__ == '__main__':
+    demoTest()
+
 
